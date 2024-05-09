4ade9825340f3ff1e0ebe878c0e31d01df020979
Magdalena Kotołowska <magdalena.kotolowska@nextory.com>
2024-05-09 19:28:07 +0200

    Create a latest_price method on Package model

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,4 +4,8 @@ class Package < ApplicationRecord
   has_many :prices, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def latest_price(municipality)
+    prices.where(municipality: municipality).order(:created_at).last&.amount_cents
+  end
 end
