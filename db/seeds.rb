32ca052771ae2a02ba614ef28ae293320f45e0cd
Magdalena Kotołowska <magdalena.kotolowska@nextory.com>
2024-05-09 20:48:47 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,12 @@ premium = Package.find_by!(name: "premium")
 plus = Package.find_by!(name: "plus")
 basic = Package.find_by!(name: "basic")
 
+puts "Creating municipalities"
+
+Municipality.insert_all(
+  YAML.load_file(Rails.root.join("import/municipalities.yaml"))
+)
+
 puts "Creating a price history for the packages"
 prices = YAML.load_file(Rails.root.join("import/initial_price_history.yaml"))
 
