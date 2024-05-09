7b6795c3fd48941210d4754cfac79e8e2de0a892
Magdalena Kotołowska <magdalena.kotolowska@nextory.com>
2024-05-09 20:55:35 +0200

    Implement PriceHistory feature

--- app/lib/price_history.rb
+++ app/lib/price_history.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
 
 class PriceHistory
-  def self.call(...)
+  def self.call(year:, package_name:, municipality_name: nil)
+    prices = Price.joins(:package, :municipality)
+                  .where("strftime('%Y', prices.created_at) = ?", year) #for 'PostgreSQL' .where("EXTRACT(YEAR FROM prices.created_at) = ?")
+                  .where(packages: { name: package_name.capitalize })
+                  .order(:municipality_id, :created_at)
 
-    # Feature request 2: Pricing History
+    prices = prices.where(municipalities: { name: municipality_name.capitalize }) if municipality_name
 
-    raise NotImplementedError, "Implement this to pass the assignment"
+    format_prices(prices)
+  end
+
+  private
+
+  def self.format_prices(prices)
+    prices.each_with_object({}) do |price, result|
+      municipality = price.municipality.name
+      result[municipality] ||= []
+      result[municipality] << price.amount_cents
+    end
   end
 end
